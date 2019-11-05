aa2bb3af680b2b2a3901802f239af516a8f877d4
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2019-11-05 23:18:42 +0100

    add: scss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,15 @@
 import React from "react";
 import { ReactComponent as Logo } from "./assets/dzik.svg";
-import "./App.css";
+import "./App.scss";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo className="App-logo"></Logo>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Logo className="App__logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </main>
   );
 };
 
